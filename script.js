8674adc7b5e71791f3f5eac29dc2495184d11b4b
Silvia Negrut (intern) <silvia.negrut@3pillar.corp>
2020-08-02 10:45:24 -0700

    my first commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function firstFunction(){
+
+
 const submitForm = document.querySelector('.add');
 const addButton = document.querySelector('.addToDo');
 const ToDo = document.querySelector('.todo');
@@ -5,15 +8,16 @@ const list = document.querySelectorAll('.todo li');
 
 let listLenght = list.lenght;
 
-const generateTempalate = (todos) => {
+const generateTemplate = (todos) => {
   
   const html = `<li>
                   <input type="checkbox" id="todos_${listLenght}">
                   <label for="todos_${listLenght}">
                     <span class="check"></span>
-                    ${todo}
+                    ${todos}
                   </label>
-                  <i class="far fa-trash-alt delete"></i>
+                  <button class="delete">🗑️</button>
+
                 </li>`
   ToDo.innerHTML += html;
 };
@@ -23,18 +27,37 @@ function addTodos(e) {
   const todo = submitForm.add.value.trim();
   if (todo.length) {
     listLenght = listLenght + 1;
-    generateTempalate(todo);
+    generateTemplate(todo);
     submitForm.reset();
+    saveTodos();
   }
 }
 
 submitForm.addEventListener('submit', addTodos);
 addButton.addEventListener('click', addTodos);
 
+
+
 function deleteTodos(e) {
+  console.log(e.target);
   if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
+      e.target.parentElement.remove();
+      saveTodos();
   }
 }
 
 ToDo.addEventListener('click', deleteTodos);
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+  firstFunction();
+});
+
+
+
+//save data to local storage
+function saveTodos(){
+  var str=JSON.stringify(todos);
+  localStorage.setItem("todos", str);
+}
+
+
