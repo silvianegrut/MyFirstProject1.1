f4622ceefe55ad429e0c66758cab44d9d9cfff2c
Silvia Negrut (intern) <silvia.negrut@3pillar.corp>
2020-08-09 23:10:08 -0700

    d

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
-const submitForm = document.querySelector('.add');
-const addButton = document.querySelector('.addToDo');
-const ToDo = document.querySelector('.todo');
-const list = document.querySelectorAll('.todo li'); 
+function firstFunction(){
 
-let listLenght = list.lenght;
+  const submitForm = document.querySelector('.add');
+  const addButton = document.querySelector('.addToDo');
+  const ToDo = document.querySelector('.todo');
 
-const generateTempalate = (todos) => {
+
+  const generateTemplate = (todo) => {
+    const list = document.querySelectorAll('.todo li'); //NodeList of all li's
+    let listLength = list.length;
+    console.log(listLength);
+
   
-  const html = `<li>
-                  <input type="checkbox" id="todos_${listLenght}">
-                  <label for="todos_${listLenght}">
+    const html = `<li>
+                  <input type="checkbox" id="todos_${todo.id}">
+                  <label for="todos_${listLength}">
                     <span class="check"></span>
-                    ${todo}
+                    ${todo.value}
                   </label>
-                  <i class="far fa-trash-alt delete"></i>
-                </li>`
-  ToDo.innerHTML += html;
+                  <button data-id="${todo.id}" class="delete">🗑️</button>
+                  </li>`
+    ToDo.innerHTML += html;
 };
 
-function addTodos(e) {
-  e.preventDefault();
-  const todo = submitForm.add.value.trim();
-  if (todo.length) {
-    listLenght = listLenght + 1;
-    generateTempalate(todo);
-    submitForm.reset();
+
+  function addTodos(e) {
+    e.preventDefault();
+    const todo = submitForm.add.value.trim(); //trim the extra space in input
+    if (todo.length) {
+        const newToDo={id:Date.now(),value:todo};
+        generateTemplate(newToDo);
+        submitForm.reset();
+        var storedTodo=JSON.parse(localStorage.getItem('mytodos')) || [];
+        storedTodo.push(newToDo);
+        localStorage.setItem('mytodos', JSON.stringify(storedTodo));
   }
-}
+ }
+  //add todo
+  submitForm.addEventListener('submit', addTodos);
+  addButton.addEventListener('click', addTodos);
 
-submitForm.addEventListener('submit', addTodos);
-addButton.addEventListener('click', addTodos);
-
-function deleteTodos(e) {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
+  function deleteTodos(e) {
+    console.log(e.target);
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+        const id=+e.target.dataset.id;
+        var storedTodo=JSON.parse(localStorage.getItem('mytodos')) || [];
+        storedTodo=storedTodo.filter((x) => x.id !=id )
+        console.log(storedTodo);
+        localStorage.setItem('mytodos', JSON.stringify(storedTodo));
+    }
   }
-}
+  //remove todo
+  ToDo.addEventListener('click', deleteTodos);
 
-ToDo.addEventListener('click', deleteTodos);
+     var todos=JSON.parse(localStorage.getItem('mytodos'));
+     console.log('todos',todos);
+     todos.forEach(todo => {
+       generateTemplate(todo);
+  });
+
+}
+     window.addEventListener('DOMContentLoaded', (event) => {
+       firstFunction();
+});
+
+
+
+
+
+
+
+
